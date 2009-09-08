f66d816be22d6938f15b81a1af3f25f12b54e947
yoppi <y.hirokazu@gmail.com>
2009-09-08 09:31:47 +0900

    rubygemsをrequireする必要がある。またモジュールのクラス名を変更したため修正

--- lib/endlessdns.rb
+++ lib/endlessdns.rb
@@ -3,13 +3,14 @@ unless $LOAD_PATH.include? File.dirname(__FILE__)
 end
 
 require 'thread'
+require 'rubygems'
 
 require 'endlessdns/engine'  # system controller
 require 'endlessdns/log'     # system log
 require 'endlessdns/statistics' # DNS query statistics
 require 'endlessdns/table'   # to recache table
 require 'endlessdns/timer'   # TTL timer
-require 'endlessdns/spoof'   # packet spoofing
+require 'endlessdns/snoop'   # packet snooping
 require 'endlessdns/packet'  # packet queue
 require 'endlessdns/version' # version of system
 
