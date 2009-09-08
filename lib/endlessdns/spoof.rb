a476cb3fb22d3cc18c41a4b30861af6c8a9abba0
yoppi <y.hirokazu@gmail.com>
2009-09-08 09:14:42 +0900

    コンストラクタおよびdumpメソッドを追加

--- lib/endlessdns/spoof.rb
+++ lib/endlessdns/spoof.rb
@@ -1,6 +1,30 @@
 require 'pcap'
+require 'net/dns/packet'
 
 module EndlessDNS
-  class Spoof
+
+  attr_reader :device
+
+  class Snoop
+    def initialize(device=nil, snaplen=1518, promisc=0, to_ms=1000)
+      unless device
+        @device = Pcap.lookupdev
+      end
+      @handle = Pcap::Capture.open(device, snaplen, promisc, to_ms)
+    end
+
+    def dump(count=-1)
+      # NOTE: 途中でdumpのstop、またstratができるようにする
+      #       そうすると、何かしらのrdbmsを使わないと、学習データを残せない
+      #       汎用的にするために、ActiveRecordを使って、依存しないようにする
+      @handle.each_packet(count) do |pkt|
+         packet.enq(pkt)
+      end
+      @handle.close
+    end
+
+    def setfileter(filter, optimize=false)
+      @handle.setfileter(filter, optimize)
+    end
   end
 end
