85f4171cc6b244d1e82202180f4dea2acb8ad2f8
yoppi <y.hirokazu@gmail.com>
2009-09-08 09:16:26 +0900

    キューの実装を加えた

--- lib/endlessdns/packet.rb
+++ lib/endlessdns/packet.rb
@@ -1,4 +1,25 @@
 module EndlessDNS
   class Packet
+    class << self
+      def instance
+        @instance ||= new
+      end
+    end
+
+    def initialize
+      @queue = Queue.new
+    end
+
+    def deq
+      @queue.deq
+    end
+
+    def enq(obj)
+      @queue.enq(obj)
+    end
   end
+end
+
+def packet
+  EndlessDNS::Packet.instance
 end
