46d02c96f26418ef48a7d0e3c8173332cc57e20d
yoppi <y.hirokazu@gmail.com>
2009-10-22 00:04:19 +0900

    統計情報ページのCGI部分の作成に着手

--- lib/web/cgi/statistics.rb
+++ lib/web/cgi/statistics.rb
@@ -6,12 +6,46 @@
 #   o hit率
 #   o クエリの散布図
 require 'cgi'
+require 'erb'
+require 'frontcgi'
 require 'menu'
 
 class Statistics
   include MenuHelper
-  
-  def initialize
-    
+  PERIOD = 60 * 60 * 12
+
+  def initialize(cgi)
+    @cgi = cgi
+    @selected = "statistics"
+  end
+
+  def get_statistics
+    now = Time.now
+    period = get_period()
+  end
+
+  def setup
+
+  end
+
+  def out
+    @cgi.out {
+      to_html
+    }
+  end
+
+  def to_html
+
+  end
+
+  def get_period
+    # GETかPOSTで表示期間が指定されていればそれを使う
+    @cgi[]
   end
 end
+
+cgi = CGI.new
+stats = Home.new(cgi)
+stats.get_statistics
+stats.setup
+stats.out
