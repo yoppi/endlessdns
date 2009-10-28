3506372de9a8edf255630b5cac30a59f3fae92da
yoppi <y.hirokazu@gmail.com>
2009-10-28 20:33:29 +0900

    config画面の開発に着手

--- lib/web/cgi/config.rb
+++ lib/web/cgi/config.rb
@@ -8,7 +8,45 @@ require 'menu'
 
 class Config
   include MenuHelper
+  include ERB::Util
 
-  def initialize
+  def initialize(cgi)
+    @cgi = cgi
+    @selected = 'config'
+  end
+
+  def do_request
+    if post?
+      do_post
+    else
+      do_get
+    end
+  end
+
+  def do_post
+    # ユーザからの設定の変更を処理してそのままページを返す
+  end
+
+  def do_get
+    # 現在の設定を返す
+  end
+
+  def setup
+
+  end
+
+  def out
+    @cgi.out {
+      to_html
+    }
+  end
+
+  def to_html
   end
 end
+
+cgi = CGI.new
+config = Config.new(cgi)
+config.do_request
+config.setup
+config.out
