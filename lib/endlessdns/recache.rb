64421d659b48d20b1822d4871cc13db3b289d64e
yoppi <y.hirokazu@gmail.com>
2009-12-08 08:01:54 +0900

    計算の優先順位の間違いを修正

--- lib/endlessdns/recache.rb
+++ lib/endlessdns/recache.rb
@@ -137,7 +137,7 @@ module EndlessDNS
       #日付のnormalize
       _now = Time.local(now.year, now.month, now.day)
       _begin_t = Time.local(info['begin_t'].year, info['begin_t'].month, info['begin_t'].day)
-      elapse_day = _now - _begin_t / 86400 + 1
+      elapse_day = (_now - _begin_t) / 86400 + 1
       qnday_prob = info['qnday'] / elapse_day.to_f
       qntz_prob = info['qntz_total'] ? (info['qntz_total']/info['qnday'].to_f)/24.0 : info['qntz'].size / 24.0
       qnday_prob * qntz_prob
